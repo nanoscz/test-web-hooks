368f5c2890704d462ea7242332f85afeb97bb9b2
fernandocto.scz <fernandocto.scz@gmail.com>
2025-01-26 14:52:58 -0400

    add GitHub webhook signature verification middleware to server

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -3,17 +3,20 @@ import { envs } from "../config/envs"
 import { GithubController } from "./github/controller"
 import { GithubService } from "./services/github.service"
 import { DiscordService } from "./services/discord.service"
+import { GithubSha256Middleware } from "./middleware/github-sha256.middleware"
 
 export class Server {
   static start() {
     const app = express()
     app.use(express.json())
 
+
     const githubService = new GithubService()
     const discordService = new DiscordService({ url: envs.DISCORD_WEBHOOK_URL });
     const githubController = new GithubController(githubService, discordService);
+    const githubSha256Middleware = new GithubSha256Middleware({ secret: envs.GITHUB_WEBHOOK_SECRET });
 
-    app.post("/api/github", githubController.webhookHandler)
+    app.post("/api/github", [githubSha256Middleware.verifySignature], githubController.webhookHandler)
     app.use("*", (req: Request, res: Response) => {
       res.send("Welcome!")
     })
