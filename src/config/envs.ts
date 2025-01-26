0f5fbeb35348fe5a964952bdf9128e6e4747f36c
fernandocto.scz <fernandocto.scz@gmail.com>
2025-01-26 14:52:30 -0400

    add .env.template and update envs.ts to include GITHUB_WEBHOOK_SECRET

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,4 +5,5 @@ import { get } from "env-var"
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
   DISCORD_WEBHOOK_URL: get("DISCORD_WEBHOOK_URL").required().asString(),
+  GITHUB_WEBHOOK_SECRET: get("GITHUB_WEBHOOK_SECRET").required().asString(),
 }
